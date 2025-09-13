d60577384623862a8cae2468018bf5a29934183f
Naipawat Poolsawat <tata-naipawat@hotmail.com>
2025-09-13 20:04:27 +0700

    Refactor Todo record to include validation constraints and define interfaces for CRUD operations. Added @NotNull and @NotBlank annotations for id and title fields, respectively.

--- src/main/java/dev/boraxpr/quarkus/todo/Todo.java
+++ src/main/java/dev/boraxpr/quarkus/todo/Todo.java
@@ -2,10 +2,28 @@ package dev.boraxpr.quarkus.todo;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record Todo(
-    Integer id,
-    String title,
-    String description,
-    boolean done,
-    LocalDateTime created_at,
-    LocalDateTime updated_at) {}
+                @NotNull(message = "Id is required", groups = {
+                                UpdateTodo.class, DeleteTodo.class }) Integer id,
+                @NotBlank(message = "Title is required", groups = { CreateTodo.class }) String title,
+                String description,
+                boolean done,
+                LocalDateTime created_at,
+                LocalDateTime updated_at){
+
+        public interface CreateTodo {
+        }
+
+        public interface UpdateTodo {
+        }
+
+        public interface DeleteTodo {
+        }
+
+        public interface ReadTodo {
+        }
+
+}
